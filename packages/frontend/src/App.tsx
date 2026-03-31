1f1f53116ce39b7ca121a2648756c01b363389a0
Sai ko <37075497+koichi1115@users.noreply.github.com>
2026-03-31 14:15:21 +0900

    fix: prevent infinite loop in App useEffect

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -99,7 +99,8 @@ function App() {
 
   useEffect(() => {
     checkAuth();
-  }, [checkAuth]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <BrowserRouter>
